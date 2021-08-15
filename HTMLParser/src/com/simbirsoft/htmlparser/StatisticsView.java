a035adabe69cc17f96aa6f2876f8e97553cdf66a
Igor Miheev <fathalblow@gmail.com>
2021-08-15 18:27:20 +0400

    add sort to StatisticsView

--- HTMLParser/src/com/simbirsoft/htmlparser/StatisticsView.java
+++ HTMLParser/src/com/simbirsoft/htmlparser/StatisticsView.java
@@ -1,17 +1,21 @@
 package com.simbirsoft.htmlparser;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
 
 public class StatisticsView {
 	public void printStatistics(HashMap<String, Long> statisticsInstance) throws IOException {
-		// TODO Auto-generated method stub
 		try {
 			System.out.println("Statistics by words:");
-			System.out.println(statisticsInstance);
-
+			List<Entry<String, Long>> list = new ArrayList<Entry<String, Long>>(statisticsInstance.entrySet());
+			list.sort(Entry.<String, Long>comparingByValue().reversed());
+			for (Entry<String, Long> entry : list) {
+				System.out.println(entry.getKey() + " - " + entry.getValue());
+			}
 		} catch (Exception e) {
-			// TODO: handle exception
 			System.out.println(e.getMessage());
 
 		}
