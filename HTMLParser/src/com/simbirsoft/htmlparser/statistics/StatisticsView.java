5b361229cc42ae61b8394c34e7921b77611ccff4
Igor Miheev <fathalblow@gmail.com>
2021-08-15 20:32:52 +0400

    remove try Catch from StatisticsView

--- HTMLParser/src/com/simbirsoft/htmlparser/statistics/StatisticsView.java
+++ HTMLParser/src/com/simbirsoft/htmlparser/statistics/StatisticsView.java
@@ -8,17 +8,13 @@ import java.util.Map.Entry;
 
 public class StatisticsView {
 	public void printStatistics(HashMap<String, Long> statisticsInstance) throws IOException {
-		try {
-			System.out.println("Statistics by words:");
-			List<Entry<String, Long>> list = new ArrayList<Entry<String, Long>>(statisticsInstance.entrySet());
-			list.sort(Entry.<String, Long>comparingByValue().reversed());
-			for (Entry<String, Long> entry : list) {
-				System.out.println(entry.getKey() + " - " + entry.getValue());
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
+		System.out.println("Statistics by words:");
 
+		List<Entry<String, Long>> list = new ArrayList<Entry<String, Long>>(statisticsInstance.entrySet());
+		list.sort(Entry.<String, Long>comparingByValue().reversed());
+
+		for (Entry<String, Long> entry : list) {
+			System.out.println(entry.getKey() + " - " + entry.getValue());
 		}
 	}
-
 }
