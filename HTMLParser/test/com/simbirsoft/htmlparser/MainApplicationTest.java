50a70f767044a4b320869a7b5ab95bdd6a37f146
Develop <Develop@LAPTOP-SH4CNLRV>
2021-08-19 15:24:05 +0400

    fix test create and write log file

--- HTMLParser/test/com/simbirsoft/htmlparser/MainApplicationTest.java
+++ HTMLParser/test/com/simbirsoft/htmlparser/MainApplicationTest.java
@@ -1,7 +1,9 @@
 package com.simbirsoft.htmlparser;
 
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -11,7 +13,16 @@ class MainApplicationTest {
 
 	@Test
 	void testMain() {
-		assertTrue(Files.isWritable(Paths.get(MainApplication.logFileName)));
+		if (Files.exists(Paths.get(MainApplication.logFileName))) {
+			assertTrue(Files.isWritable(Paths.get(MainApplication.logFileName)));
+		} else {
+			try {
+				Files.createFile(Paths.get(MainApplication.logFileName));
+			} catch (IOException e) {
+				fail("can't create log file");
+			}
+			assertTrue(Files.isWritable(Paths.get(MainApplication.logFileName)));
+		}
 	}
 
 }
