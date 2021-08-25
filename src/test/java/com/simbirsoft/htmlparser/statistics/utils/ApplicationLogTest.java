6f2d80148b7534a161ee14937a4e512653a6a9b1
Igor Miheev <fathalblow@gmail.com>
2021-08-26 02:23:40 +0400

    move src folder and adding resource dependency in pom.xml

---
+++ src/test/java/com/simbirsoft/htmlparser/statistics/utils/ApplicationLogTest.java
@@ -0,0 +1,36 @@
+package test.java.com.simbirsoft.htmlparser.statistics.utils;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+
+import main.java.com.simbirsoft.htmlparser.Settings;
+
+class ApplicationLogTest {
+
+	@Test
+	void TestApplicationLog() {
+		Settings settings = new Settings();
+		settings.loadFromClassLoader("app.properties");
+		String logFileName = settings.getValue("logFileName");
+
+		if (Files.exists(Paths.get(logFileName))) {
+			assertTrue(Files.isWritable(Paths.get(logFileName)));
+		} else {
+			try {
+				Files.createDirectories(Paths.get(logFileName).getParent());
+				Files.createFile(Paths.get(logFileName));
+				// Files.
+			} catch (IOException e) {
+				fail("Can't create log file");
+			}
+			assertTrue(Files.isWritable(Paths.get(logFileName)));
+		}
+	}
+
+}
